93e7e1b596184aef47862b9c0a597098fc689cb6
unknown <Japatius_96@hotmail.com>
2019-03-25 15:32:28 +0200

    Navigation bar done

--- app/src/main/java/calendar/esports/MainActivity.java
+++ app/src/main/java/calendar/esports/MainActivity.java
@@ -1,13 +1,58 @@
 package calendar.esports;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        //fragments
+        final HomeFragment homeFragment = new HomeFragment();
+        final HubFragment hubFragment = new HubFragment();
+        final CalendarFragment calendarFragment = new CalendarFragment();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.home){
+                    setFragment(homeFragment);
+                    return true;
+                } else if (id == R.id.directory){
+                    setFragment(hubFragment);
+                    return true;
+                } else if (id == R.id.calendar) {
+                    setFragment(calendarFragment);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
     }
 }
