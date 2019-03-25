1c8e4db7b3e6b7f6d934f2425ac5eee7db5d0070
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-03-25 12:45:07 +0200

    Display match by game

---
+++ app/src/main/java/trung/nguyen/icongame/Models.kt
@@ -0,0 +1,33 @@
+package trung.nguyen.icongame
+
+import java.util.*
+
+//[
+//{
+//    winner_id: null,
+//    winner: null,
+//    videogame_version: {},
+//    videogame: {},
+//    tournament_id: 2088,
+//    tournament: {},
+//    status: "running",
+//    slug: "apk-prince-vs-bbq-olivers-2019-03-18",
+//    serie_id: 1713,
+//    serie: {},
+//    results: [],
+//    opponents: [],
+//    number_of_games: 3,
+//    name: "APK vs BBQ",
+//    modified_at: "2019-03-18T11:30:49Z",
+//    match_type: "best_of",
+//    live: {},
+//    league_id: 4142,
+//    league: {},
+//    id: 541085,
+//    games: [],
+//    draw: false,
+//    begin_at: "2019-03-18T11:30:49Z",
+//},
+//]
+
+class Match(val id: Int, val name: String, val begin_at: Date)
