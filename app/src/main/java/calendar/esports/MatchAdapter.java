60c0917f6d6f5f966b83e785d76b9edb95037cf3
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-04-03 16:11:55 +0300

    Match: Match UI with League Title and Team Img Display

--- app/src/main/java/calendar/esports/MatchAdapter.java
+++ app/src/main/java/calendar/esports/MatchAdapter.java
@@ -1,28 +1,16 @@
 package calendar.esports;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.Serializable;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import static android.app.PendingIntent.getActivity;
 
@@ -47,65 +35,73 @@ public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
 
-        String matchInfo = matches[position].getName() + ": " + matches[position].getId().toString();
+        String matchHour = new SimpleDateFormat("h:mm a").format(matches[position].getBegin_at());
+        holder.matchHour.setText(matchHour);
 
-        String oldTimeString = new SimpleDateFormat("E  h:mm a").format(matches[position].getBegin_at());
-        holder.matchBegin.setText(oldTimeString);
-        holder.matchTitle.setText(matches[position].getName());
+        String matchMonth = new SimpleDateFormat("MMM").format(matches[position].getBegin_at());
+        holder.matchMonth.setText(matchMonth);
 
-        holder.matchTitle.setOnClickListener(View -> {
+        String matchDate = new SimpleDateFormat("d").format(matches[position].getBegin_at());
+        holder.matchDate.setText(matchDate);
 
-                Toast.makeText(context, (CharSequence) matchInfo, Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(context, MatchDetails.class);
-                intent.putExtra("MatchDetail", matches[position]);
-                context.startActivity(intent);
+        String matchWeekDay = new SimpleDateFormat("E").format(matches[position].getBegin_at());
+        holder.matchWeekDay.setText(matchWeekDay);
 
-        });
-
-        holder.notificationIcon.setOnClickListener(new View.OnClickListener() {
-            int notificationPos = 0;
-            public void onClick(View view) {
-
-                if(notificationPos == 0){
-                    holder.notificationIcon.setImageResource(R.drawable.ic_notifications_active_black_24dp);
-                    notificationPos = 1;
-
-                    Toast.makeText(context, (CharSequence) matches[position].getBegin_at()
-                            .toString(), Toast.LENGTH_SHORT).show();
-                }
-
-                else if (notificationPos == 1){
-                    holder.notificationIcon.setImageResource(R.drawable.ic_notifications_black_24dp);
-                    notificationPos = 0;
-                }
-                //Function to add event to the calendar (with bundle? or args? or import calendar?)
-                String time = matches[position].getBegin_at().toString();
-                Log.d("MATCHTIME", "onClick: " + time);
-                SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
-                try {
-                    Date mDate = sdf.parse(time);
-                    long timeInMilliseconds = mDate.getTime();
-                    Log.d("MATCHTIME", "onClick: " + timeInMilliseconds);
-
-//                    Bundle args = new Bundle();
-//                    args.putLong("time", timeInMilliseconds);
-//                    CalendarFragment fragobj = new CalendarFragment();
-//                    fragobj.setArguments(args);
+//        String oldTimeString = new SimpleDateFormat("E  h:mm a").format(matches[position].getBegin_at());
+//        holder.matchBegin.setText(oldTimeString);
+//        holder.matchTitle.setText(matches[position].getName());
 //
-//                    FragmentManager fragmentManager = ((AppCompatActivity)context).getSupportFragmentManager();
-//                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                    fragmentTransaction.replace(R.id.header_text, fragobj);
-//                    fragmentTransaction.addToBackStack(null);
-//                    fragmentTransaction.commit();
-
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-        });
-
+//        holder.matchTitle.setOnClickListener(View -> {
+//
+//                Toast.makeText(context, (CharSequence) matchInfo, Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(context, MatchDetails.class);
+//                intent.putExtra("MatchDetail", matches[position]);
+//                context.startActivity(intent);
+//
+//        });
+//
+//        holder.notificationIcon.setOnClickListener(new View.OnClickListener() {
+//            int notificationPos = 0;
+//            public void onClick(View view) {
+//
+//                if(notificationPos == 0){
+//                    holder.notificationIcon.setImageResource(R.drawable.ic_notifications_active_black_24dp);
+//                    notificationPos = 1;
+//
+//                    Toast.makeText(context, (CharSequence) matches[position].getBegin_at()
+//                            .toString(), Toast.LENGTH_SHORT).show();
+//                }
+//
+//                else if (notificationPos == 1){
+//                    holder.notificationIcon.setImageResource(R.drawable.ic_notifications_black_24dp);
+//                    notificationPos = 0;
+//                }
+//                //Function to add event to the calendar (with bundle? or args? or import calendar?)
+//                String time = matches[position].getBegin_at().toString();
+//                Log.d("MATCHTIME", "onClick: " + time);
+//                SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
+//                try {
+//                    Date mDate = sdf.parse(time);
+//                    long timeInMilliseconds = mDate.getTime();
+//                    Log.d("MATCHTIME", "onClick: " + timeInMilliseconds);
+//
+////                    Bundle args = new Bundle();
+////                    args.putLong("time", timeInMilliseconds);
+////                    CalendarFragment fragobj = new CalendarFragment();
+////                    fragobj.setArguments(args);
+////
+////                    FragmentManager fragmentManager = ((AppCompatActivity)context).getSupportFragmentManager();
+////                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+////                    fragmentTransaction.replace(R.id.header_text, fragobj);
+////                    fragmentTransaction.addToBackStack(null);
+////                    fragmentTransaction.commit();
+//
+//                } catch (ParseException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        });
 
     }
 
@@ -117,17 +113,43 @@ public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView matchBegin;
-        TextView matchTitle;
+        TextView matchMonth;
+        TextView matchDate;
+        TextView matchWeekDay;
+
+        TextView  matchLeague;
+        ImageView matchTeam1Img;
+        ImageView matchTeam2Img;
+        TextView  matchTeam1;
+        TextView  matchTeam2;
+
         RelativeLayout parentLayout;
+        RelativeLayout timeGroup;
+        RelativeLayout notificationGroup;
+
         ImageView notificationIcon;
+        TextView  matchHour;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            matchBegin       = itemView.findViewById(R.id.textView_match_time);
-            matchTitle       = itemView.findViewById(R.id.textView_match_title);
-            parentLayout     = itemView.findViewById(R.id.parent_layout);
-            notificationIcon = itemView.findViewById(R.id.notification_icon);
+            matchMonth        = itemView.findViewById(R.id.textView_match_month);
+            matchDate         = itemView.findViewById(R.id.textView_match_date);
+            matchWeekDay      = itemView.findViewById(R.id.textView_match_weekday);
+
+            matchLeague       = itemView.findViewById(R.id.textView_match_league_title);
+            matchTeam1        = itemView.findViewById(R.id.textView_match_team1);
+            matchTeam2        = itemView.findViewById(R.id.textView_match_team2);
+            matchTeam1Img     = itemView.findViewById(R.id.imageView_team1_img);
+            matchTeam2Img     = itemView.findViewById(R.id.imageView_team2_img);
+
+
+            notificationIcon  = itemView.findViewById(R.id.notification_icon);
+            matchHour         = itemView.findViewById(R.id.textView_match_hour);
+
+            parentLayout      = itemView.findViewById(R.id.parent_layout);
+            timeGroup         = itemView.findViewById(R.id.time_group);
+            notificationGroup = itemView.findViewById(R.id.notification_group);
+
         }
     }
 
