3955360a34fe4020900d72390f1fdb582ff1597f
Jaakko <Japatius_96@hotmail.com>
2019-04-09 09:33:37 +0300

    merge

---
+++ app/src/main/java/calendar/esports/Instructions/CreditsFragment.java
@@ -0,0 +1,30 @@
+package calendar.esports.Instructions;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import calendar.esports.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreditsFragment extends Fragment {
+
+
+    public CreditsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_credits, container, false);
+    }
+
+}
