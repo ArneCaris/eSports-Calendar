184359c56847d1d2d48689b03ef0bd59964ef45e
Japatius <Japatius_96@hotmail.com>
2019-04-25 02:27:51 +0300

    Replaced carousel, splash screen fixed

--- app/src/main/java/calendar/esports/HomeFragment.java
+++ app/src/main/java/calendar/esports/HomeFragment.java
@@ -21,10 +21,6 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
-import com.synnapps.carouselview.CarouselView;
-import com.synnapps.carouselview.ImageClickListener;
-import com.synnapps.carouselview.ImageListener;
-import com.synnapps.carouselview.ViewListener;
 
 import calendar.esports.MainActivity;
 import calendar.esports.Model.Points;
@@ -40,10 +36,6 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    private int[] mImages = new int[] {
-        R.drawable.placeholder, R.drawable.teo_wide, R.drawable.estream
-    };
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,48 +43,11 @@ public class HomeFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        CarouselView carouselView = (CarouselView) v.findViewById(R.id.carouselView);
-        carouselView.setPageCount(mImages.length);
-        carouselView.setImageListener(new ImageListener() {
-            @Override
-            public void setImageForPosition(int position, ImageView imageView) {
-                imageView.setImageResource(mImages[position]);
-            }
-        });
-
-
-        carouselView.setImageClickListener(new ImageClickListener() {
-            @Override
-            public void onClick(int position) {
-                String csgoNews = "https://www.hltv.org/";
-                String owNews = "https://playoverwatch.com/en-us/news";
-                switch (position) {
-                    case 0:
-                        Intent i = new Intent(getActivity(), NewsActivity.class);
-                        startActivity(i);
-                        break;
-                    case 1:
-                        Uri uri = Uri.parse(csgoNews);
-                        Intent ii = new Intent(Intent.ACTION_VIEW, uri);
-                        startActivity(ii);
-                        break;
-                    case 2:
-                        Uri uri1 = Uri.parse(owNews);
-                        Intent in = new Intent(Intent.ACTION_VIEW, uri1);
-                        startActivity(in);
-                        break;
-                }
-
-
-
-                /*if (position == 0) {
-                    Intent i = new Intent(getActivity(), NewsActivity.class);
-                    startActivity(i);
-                }*/
-            }
-        });
-
-
+        CardView cardView = (CardView) v.findViewById(R.id.esport_news);
+        cardView.setOnClickListener((view -> {
+            Intent i = new Intent(getActivity(), NewsActivity.class);
+            startActivity(i);
+        }));
 
         return v;
     }
