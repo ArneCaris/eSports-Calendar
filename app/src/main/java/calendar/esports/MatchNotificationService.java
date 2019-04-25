0858b54e35099e1b4a966b9722f8c497acca04d0
Jaakko <Japatius_96@hotmail.com>
2019-04-25 12:43:29 +0300

    Merge conflict fixed in MatchNotificationService

--- app/src/main/java/calendar/esports/MatchNotificationService.java
+++ app/src/main/java/calendar/esports/MatchNotificationService.java
@@ -7,10 +7,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-<<<<<<< HEAD
-=======
 import android.net.Uri;
->>>>>>> 01ffed999b58c3a0ff5996e996489676bbd0a4d1
 import android.os.Build;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
@@ -26,7 +23,6 @@ public class MatchNotificationService extends Service {
     private final String CHANNEL_ID = "estream_notifications";
     public static int MY_NOTIFICATION_ID;
     private NotificationManager notificationManager;
-    private String CHANNEL_ID = "start_notification";
 
     @Override
     public void onCreate() {
@@ -40,16 +36,13 @@ public class MatchNotificationService extends Service {
         if(serializable instanceof Match) {
             Match match           = (Match) serializable;
             MY_NOTIFICATION_ID    = match.getId();
-<<<<<<< HEAD
 
             Log.d("matchName", "onReceive Match: " + match.getName());
             Log.d("gameIcon", "onReceive: " + gameIcon);
             createNotificationChannel();
-            sendNotification("Match Start", gameIcon, match);
-=======
-//            createNotificationChannel();
+////            createNotificationChannel();
             sendNotification(gameIcon, match);
->>>>>>> 01ffed999b58c3a0ff5996e996489676bbd0a4d1
+
         }
 
         return START_NOT_STICKY;
@@ -59,11 +52,7 @@ public class MatchNotificationService extends Service {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "EStream Notifications";
             String description = "All EStream notifications";
-<<<<<<< HEAD
-            int importance = notificationManager.IMPORTANCE_MIN;
-=======
             int importance = notificationManager.IMPORTANCE_DEFAULT;
->>>>>>> 01ffed999b58c3a0ff5996e996489676bbd0a4d1
             NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, importance);
             notificationChannel.setDescription(description);
             notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
@@ -71,14 +60,8 @@ public class MatchNotificationService extends Service {
         }
     }
 
-<<<<<<< HEAD
-    private void sendNotification(String match_start, String gameIcon, Match match) {
-        notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        String timeOfEvent = new SimpleDateFormat("dd-MM-yyyy hh:mm a", Locale.ENGLISH).format(match.getBegin_at());
-        String defLogo = "game_logo1" ;
-=======
-    private void sendNotification(String gameIcon, Match match) {
 
+    private void sendNotification(String gameIcon, Match match) {
         notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
         String timeOfEvent = new SimpleDateFormat("dd-MM-yyyy hh:mm a", Locale.ENGLISH).format(match.getBegin_at());
         String defLogo = "game_logo1";
@@ -87,22 +70,10 @@ public class MatchNotificationService extends Service {
         String urlOw = "https://www.twitch.tv/directory/game/Overwatch";
         String urlDota2 = "https://www.twitch.tv/directory/game/Dota%202";
         Intent streamIntent = new Intent(Intent.ACTION_VIEW);
->>>>>>> 01ffed999b58c3a0ff5996e996489676bbd0a4d1
 
         switch (gameIcon) {
             case "csgo":
                 defLogo = defLogo.replace("1", "2");
-<<<<<<< HEAD
-                break;
-            case "lol":
-                defLogo = defLogo.replace("1", "5");
-                break;
-            case "ow":
-                defLogo = defLogo.replace("1", "4");
-                break;
-            case "dota2":
-                defLogo = defLogo.replace("1", "3");
-=======
                 streamIntent.setData(Uri.parse(urlCsgo));
                 //uri.parse("https://twitch.tv/ + channelName")
                 break;
@@ -117,7 +88,6 @@ public class MatchNotificationService extends Service {
             case "dota2":
                 defLogo = defLogo.replace("1", "3");
                 streamIntent.setData(Uri.parse(urlDota2));
->>>>>>> 01ffed999b58c3a0ff5996e996489676bbd0a4d1
                 break;
         }
 
@@ -131,21 +101,14 @@ public class MatchNotificationService extends Service {
                 1, streamIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         Notification myNotification = new NotificationCompat.Builder(this, CHANNEL_ID)
-<<<<<<< HEAD
-                .setContentTitle("\uD83D\uDD14 " + match.getLeague().getName())
-=======
-                .setContentTitle(match.getLeague().getName().toString())
->>>>>>> 01ffed999b58c3a0ff5996e996489676bbd0a4d1
+
+                .setContentTitle(match.getLeague().getName())
                 .setContentText(message)
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
                 .setTicker("Notification!")
                 .setWhen(System.currentTimeMillis())
                 //.setDefaults(Notification.DEFAULT_SOUND)
-<<<<<<< HEAD
-                .setAutoCancel(true)
-=======
                 //.setAutoCancel(true)
->>>>>>> 01ffed999b58c3a0ff5996e996489676bbd0a4d1
                 .setSmallIcon(gameIdentifier)
 //                .setOngoing(true)
                 .setContentIntent(pendingStreamIntent)
